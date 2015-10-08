9d0613a8a1597f3f96ce3e67ef734e3e6d73c982
Thomas Zilliox <thomas@zilliox.me>
2015-10-08 13:18:23 +0200

    Add Sass compilation

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require("gulp-rename");
+
+gulp.task('sass', function () {
+  gulp.src('./src/gridbox.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build'))
+    .pipe(minifyCss())
+    .pipe(rename('gridbox.min.css'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(['./src/*.scss','./src/**/*.scss'], ['sass']);
+});
+
+gulp.task('default', function() {
+  gulp.run('sass');
+});
